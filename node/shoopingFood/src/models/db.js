205571090175937a8db7fa05a0f410faea3ce37f
GetMetamorph <drisskiba78@hotmail.fr>
2020-11-21 18:05:18 +0100

    Update Register / login , Add: auth.js
    Ajout css / html sur register et login
    Auth.js

--- node/shoopingFood/src/models/db.js
+++ node/shoopingFood/src/models/db.js
@@ -11,8 +11,12 @@ const connection = mysql.createConnection({
 
 // open the MySQL connection
 connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
+    if (error) {
+        console.log(error);
+    } 
+    else {
+        console.log("Successfully connected to the database.");
+    }
 });
 
 module.exports = connection;
