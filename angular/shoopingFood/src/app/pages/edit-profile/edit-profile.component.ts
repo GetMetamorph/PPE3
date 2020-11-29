851de655fd1f7ad2bd18ac15e8504dee4c8f645f
flo 2400 <florian.leg@free.fr>
2020-11-29 18:33:56 +0100

    edit profile

--- angular/shoopingFood/src/app/pages/edit-profile/edit-profile.component.ts
+++ angular/shoopingFood/src/app/pages/edit-profile/edit-profile.component.ts
@@ -1,5 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import * as jwt from 'jsonwebtoken';
+import $ from 'jquery';
 
+
+function getCookie(name) {
+  var cookieArr = document.cookie.split(";");
+  for(var i = 0; i < cookieArr.length; i++) {
+      var cookiePair = cookieArr[i].split("=");
+      if(name == cookiePair[0].trim()) {
+          return decodeURIComponent(cookiePair[1]);
+      }
+  }
+  return null;
+}
+
+function GetUser() {
+  var token = getCookie("jwt");
+  var user = jwt.decode(token);
+  return user
+}
+
+function LoadProfile(user) {
+  document.getElementById("form_editP").setAttribute("action",`http://localhost:8001/user/${user.USR_Id}`)
+  document.getElementById("exampleFirstname").setAttribute("placeholder",`${user.USR_Firstname}`)
+  document.getElementById("exampleInputEmail1").setAttribute("placeholder",`${user.USR_Mail}`)
+
+}
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
@@ -10,6 +36,7 @@ export class EditProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    LoadProfile(GetUser())
   }
 
 }
